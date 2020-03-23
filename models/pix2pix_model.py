4469ffd99d27157cceb7bdab58dabcf3a49e07a7
Niels Neerhoff <n.neerhoff@student.tudelft.nl>
2020-03-23 10:54:03 +0100

    Added docs.

--- models/pix2pix_model.py
+++ models/pix2pix_model.py
@@ -85,10 +85,12 @@ class Pix2PixModel(BaseModel):
 
     def forward(self):
         """Run forward pass; called by both functions <optimize_parameters> and <test>."""
-        self.fake_B = self.netG(self.real_A)  # G(A)
+
+        # Feed real edge map x to generator G, outputting G(x).
+        self.fake_B = self.netG(self.real_A)
 
     def backward_D(self):
-        """Calculate GAN loss for the discriminator"""
+        """ Calculate GAN loss for the discriminator. """
 
         # FAKE: Feed real edge map x and fake image G(x) to discriminator.
         fake_AB = torch.cat((self.real_A, self.fake_B), 1)
@@ -105,14 +107,17 @@ class Pix2PixModel(BaseModel):
         self.loss_D.backward()
 
     def backward_G(self):
-        """Calculate GAN and L1 loss for the generator"""
-        # First, G(A) should fake the discriminator
+        """ Calculate GAN and L1 loss for the generator. """
+
+        # Loss of G is based on # deceptions of discriminator D.
         fake_AB = torch.cat((self.real_A, self.fake_B), 1)
         pred_fake = self.netD(fake_AB)
         self.loss_G_GAN = self.criterionGAN(pred_fake, True)
-        # Second, G(A) = B
+
+        # G should produce fake image G(x) resembling y.
         self.loss_G_L1 = self.criterionL1(self.fake_B, self.real_B) * self.opt.lambda_L1
-        # combine loss and calculate gradients
+
+        # Combine losses and calculate gradients.
         self.loss_G = self.loss_G_GAN + self.loss_G_L1
         self.loss_G.backward()
 
